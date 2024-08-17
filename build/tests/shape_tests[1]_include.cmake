4974b2501cff2f8d5dbd10c93e69698738179c9d
JustBryse <brysonmacduff@gmail.com>
2024-08-17 00:04:39 -0400

    removed build contents

--- build/tests/shape_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/bryson/projects/c++/cpp-template/build/tests/shape_tests[1]_tests.cmake")
-  include("/home/bryson/projects/c++/cpp-template/build/tests/shape_tests[1]_tests.cmake")
-else()
-  add_test(shape_tests_NOT_BUILT shape_tests_NOT_BUILT)
-endif()
