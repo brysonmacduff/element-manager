57b899f5a77773b66d8b5964ee056a46080deb3a
JustBryse <brysonmacduff@gmail.com>
2024-09-15 21:05:51 -0400

    cleared out build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bryson/projects/c++/cpp-template
-# Build directory: /home/bryson/projects/c++/cpp-template/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("inc")
-subdirs("tests")
