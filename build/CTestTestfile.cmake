2eec9e74cb2ad05fbe035ca927004a315ae79951
JustBryse <brysonmacduff@gmail.com>
2024-09-23 19:47:24 -0400

    added gitignore file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/bryson/projects/c++/cpp-template
+# Build directory: /home/bryson/projects/c++/cpp-template/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("inc")
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bryson/projects/c++/cpp-template/tests
+# Build directory: /home/bryson/projects/c++/cpp-template/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/bryson/projects/c++/cpp-template/build/tests/run_tests[1]_include.cmake")
