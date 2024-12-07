838b9462159bc7d0a7c9ffe1a4f6c9fbe5daa3b3
JustBryse <brysonmacduff@gmail.com>
2024-12-07 15:40:00 -0500

    fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@ int main(int argc, char* argv[])
 {
     if(argc != 2)
     {
-        spdlog::error("main() -> Expectint 1 argument: <(int) millisecond interval>");
+        spdlog::error("main() -> Expecting 1 argument: <(int) millisecond interval>");
         return 1;
     }
 
     ElementManager elem_mngr;
     int interval_ms = std::stoi(argv[1]);
-    
+
     CycleElementManager(elem_mngr, interval_ms);
 
     return 0;
